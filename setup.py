8cfa7bd8ef4187a51ff116521d70edf0e159c99a
excelero-andreas <31904878+excelero-andreas@users.noreply.github.com>
2018-07-16 10:51:50 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='nvmesh-shell',
-    version='0.3.0',
+    version='0.3.1',
     author='Excelero, Inc. - Andreas Krause',
     url='https://github.com/Excelero/nvmesh-shell',
     description='Excelero NVMesh interactive shell and cli tool.',
